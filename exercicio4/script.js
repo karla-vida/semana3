3eed22ffe1f7b05b782195bd67615dab5a7f9c8b
karlacvida <karlacvida@hotmail.com>
2022-07-18 23:39:24 -0300

    exercicio4

---
+++ exercicio4/script.js
@@ -0,0 +1,6 @@
+let pessoaA = {nome:"João", sobrenome:"da Silva", setor:"camarote", idade:"40"}
+let pessoaB = {nome:"Marcos", sobrenome:"Hartman", setor:"camarote", idade:"17"}
+let pessoaC = {nome:"Marina", sobrenome:"Pessoa", setor:"pista", idade:"30"}
+let pessoaD = {nome:"Flávia", sobrenome:"da Costa", setor:"pista", idade:"27"}
+let pessoaE = {nome:"Arthur", sobrenome:"da Fonseca", setor:"arquibancada", idade:"20"}
+let pessoaF = {nome:"Júlia", sobrenome:"da Luz", setor:"arquibancada", idade:"16"}

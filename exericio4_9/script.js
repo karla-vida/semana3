82a7132f5ba9fcf64ccd8efc054fecf168ce7821
karlacvida <karlacvida@hotmail.com>
2022-07-18 23:45:20 -0300

    exercicio5

--- exericio4_9/script.js
+++ exericio4_9/script.js
@@ -4,3 +4,5 @@ let pessoaC = {nome:"Marina", sobrenome:"Pessoa", setor:"pista", idade:"30"}
 let pessoaD = {nome:"Flávia", sobrenome:"da Costa", setor:"pista", idade:"27"}
 let pessoaE = {nome:"Arthur", sobrenome:"da Fonseca", setor:"arquibancada", idade:"20"}
 let pessoaF = {nome:"Júlia", sobrenome:"da Luz", setor:"arquibancada", idade:"16"}
+
+var listaDeConvidados = [pessoaA, pessoaB, pessoaC, pessoaD, pessoaE, pessoaF];
